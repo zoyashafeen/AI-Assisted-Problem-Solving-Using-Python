6eefa14755ca28c756324e1503b1a1019e130f37
zoyashafeen <162790393+zoyashafeen@users.noreply.github.com>
2025-11-24 09:49:57 +0530

    Delete ASSIGNMENT 1/Task 5.py

--- ASSIGNMENT 1/Task 5.py
+++
@@ -1,8 +0,0 @@
-# FIND LARGEST NUMBER IN A LIST
-
-# Input list
-numbers = [12, 45, 2, 67, 33, 89, 23]
-
-# Method 1: Using built-in function
-largest = max(numbers)
-print("The largest number is:", largest)
